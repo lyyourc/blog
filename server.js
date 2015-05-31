6d63d725fdf623759b0cb63b1bcefdc16d9c83e1
DrakeLeung <drakeleung42@gmail.com>
2015-05-31 18:32:10 +0800

    Distribution Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('.tmp'));
+app.use(express.static('app'));
+app.use(express.static('dist'));
+
+// app.use('/', function(req, res) {
+//   // res.sendFile(__dirname + '/dist/index.html');
+// });
+
+// hllo
+
+app.use('/login', function(req, res) {
+  res.json('logined');
+});
+
+app.listen(9000);
