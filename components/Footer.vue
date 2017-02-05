be32ce23521114935d137274482caf098168c2cb
Drake Leung <drakeleung42@gmail.com>
2017-02-05 10:24:27 +0800

    :art: Tweak some default styles of Nuxt

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,3 +3,11 @@
     Visit our website for more documentation : <a href="https://nuxtjs.org" target="_blank">nuxtjs.org</a>
   </footer>
 </template>
+
+<style scoped>
+footer {
+  padding: 20px;
+  text-align: center;
+  border-top: 1px solid #ddd;
+}
+</style>
