63e2aca3256c741e4790d5153a6c3d2c76e15332
lyyourc <drakeleung42@gmail.com>
2019-03-05 00:38:12 +0800

    * style footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,12 +4,28 @@ import styled from '../../styles/styled'
 
 export default function AppFooter() {
   return (
-    <Footer>😛</Footer>
+    <Footer>
+      <Link href="https://github.com/lyyourc">GitHub</Link>
+      😛
+      <Link href="https://twitter.com/lyyourc">Twitter</Link>
+    </Footer>
   )
 }
 
 const Footer = styled('footer')`
-  height: 60px;
-  line-height: 60px;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 88px;
+  border-top: 1px solid ${props => props.theme.pallete.gray[0]};
+  padding: 10px;
+  font-size: 1.2em;
+`
+
+const Link = styled('a')`
+  padding: 0 10px;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `
